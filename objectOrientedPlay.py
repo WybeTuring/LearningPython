3d20d5861aff5d444c63420723c9e0bd79c1ef27
Wybe Turing <karllemfon@gmail.com>
2019-01-10 14:20:39 +0000

    This is simply the basis of creating a class Created a class of workers, then initialized a list with a certain number of workers, and then looped through to ensure that the internal workings were on point.

---
+++ objectOrientedPlay.py
@@ -0,0 +1,19 @@
+# My first experience with object oriented programming in python
+class worker:
+    """docstring for ."""
+    def __init__(self, name, pay): # Initialises the class, by first of all creating a new object called self
+        self.name = name; #self in this case acts like a place holder for the other variables, a safe enough aprch
+        self.pay = pay;
+    def lastName(self):
+        return self.name.split(" ")[-1];
+    def giveRise(self, percent):
+        return self.pay + self.pay*(percent/100);
+workers = [];
+# Creating a table of 10 workers in a loop
+for i in range(10):
+    identity = []
+    identity.append("Worker" + str(i));
+    identity.append(str(100*i));
+    workers.append(identity);
+for k in workers:
+    print("The {0} worker earns {1}".format(k[0], k[1]))
